f536e5c9bacab0bdf70e7d453fcce83d73d5a578
Maxime GELLE <maxime.gelle@gmail.com>
2018-03-11 22:59:17 +0100

    Refacto tests to add setup step

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,11 +1,15 @@
 defmodule KV.BucketTest do
-    use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-    test "stores values by key" do
-        {:ok, bucket} = KV.Bucket.start_link([])
-        assert KV.Bucket.get(bucket, "milk") == nil
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
 
-        KV.Bucket.put(bucket, "milk", 3)
-        assert KV.Bucket.get(bucket, "milk") == 3
-    end
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
 end
